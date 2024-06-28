98d40289a60f08fdef37bdc96baa1d954f03f849
Mukhtarhan <mukhtarhanserikbay2003@gmail.com>
2024-06-28 15:46:51 +0500

    Lab8

--- Lab7/codingBat/list-1/first_last6.py
+++ Lab7/codingBat/list-1/first_last6.py
@@ -1,2 +1,3 @@
-def first_last6(nums):
-  return nums[0] == 6 or nums[len(nums)-1] == 6
+a = [1, 2, 4]
+
+print(a[2])
--- Lab7/codingBat/list-1/has23.py
+++ Lab7/codingBat/list-1/has23.py
@@ -1,5 +1,11 @@
-def has23(nums):
-  if 2 in nums or 3 in nums:
-    return True
-  else:
-    return False
+# %%
+import folium
+
+mapObj = folium.Map(location=[43.238949,76.889709], zoom_start=11, zoom_control=False)
+
+folium.TileLayer("stamenterrain", attr='stamenterrain').add_to(mapObj)
+
+folium.LayerControl().add_to(mapObj)
+mapObj
+
+# %%
